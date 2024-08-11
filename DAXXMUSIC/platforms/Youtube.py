52f32fd5c5635f18ef0d08ac410dc409bb574020
YTFarman9758 <162802755+YTFarman9758@users.noreply.github.com>
2024-08-11 09:34:49 +0530

    Update Youtube.py

--- DAXXMUSIC/platforms/Youtube.py
+++ DAXXMUSIC/platforms/Youtube.py
@@ -11,8 +11,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from ChampuXMusic.utils.database import is_on_off
-from ChampuXMusic.utils.formatters import time_to_seconds, download_file
+from DAXXMUSIC.utils.database import is_on_off
+from DAXXMUSIC.utils.formatters import time_to_seconds, download_file
 
 
 
