edeac231a090a691ba483f9fc5cf1ebc386134e0
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-08-14 14:42:53 +0530

    Update callback.py

--- DAXXMUSIC/plugins/admins/callback.py
+++ DAXXMUSIC/plugins/admins/callback.py
@@ -3,7 +3,7 @@ import asyncio
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DAXXMUSIC import YouTube, app, YTB
+from DAXXMUSIC import YouTube, app
 from DAXXMUSIC.core.call import DAXX
 from DAXXMUSIC.misc import SUDOERS, db
 from DAXXMUSIC.utils.database import (
