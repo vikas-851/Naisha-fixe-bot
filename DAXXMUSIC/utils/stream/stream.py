bfbdcdcc4738f60e880f175cc28547e0975cc443
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-08-14 14:46:20 +0530

    Update stream.py

--- DAXXMUSIC/utils/stream/stream.py
+++ DAXXMUSIC/utils/stream/stream.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 from DAXXMUSIC.utils.database import get_assistant
 import config
-from DAXXMUSIC import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app, YTB
+from DAXXMUSIC import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
 from DAXXMUSIC.core.call import DAXX
 from DAXXMUSIC.misc import SUDOERS
 from DAXXMUSIC.utils import seconds_to_min, time_to_seconds
